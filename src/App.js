096c3859f0070f87a48a74fe7ff3eca5a53a66cb
Nora <onora155@gmail.com>
2021-07-09 12:27:52 +0530

    updated project for signup function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+import { BrowserRouter , Route } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import UpdateProduct from './UpdateProduct';
+import AddProduct from './AddProduct';
+import Protected from './Protected'
+
 function App() {
   return (
     <div className="App">
-      <h1>E-Comm Project</h1>
-      <button>Normal button</button>
-      <Button>Bootstrap button</Button>
+      <BrowserRouter>
+        
+        {/* <h1>E-Comm Project</h1> */}
+
+        <Route path="/login" ><Login /></Route>
+        <Route path="/register" ><Register /></Route>
+        <Route path="/add" ><Protected Cmp={AddProduct} /></Route>
+        <Route path="/update" ><Protected Cmp={UpdateProduct} /></Route>
+
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Register.js
@@ -0,0 +1,51 @@
+import Header from './Header';
+import React,{useState,useEffect} from 'react';
+import {useHistory} from 'react-router-dom'
+function Register(){
+
+    useEffect(()=>{
+        if(localStorage.getItem('user-info') ){
+            history.push('./add')
+        }
+    },[])
+
+    const [name,setName] = useState("");
+    const [password,setPassword] = useState("");
+    const [email,setEmail] = useState("");
+    const history = useHistory();
+    async function signUp()
+    {
+        let item ={name,password,email};
+        // console.log(item);
+        let result = await fetch("http://localhost:8000/api/register",{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body:JSON.stringify(item)
+        })
+        result = await result.json();
+        console.log("result ",result);
+        localStorage.setItem("user-info",JSON.stringify(result));
+        history.push("/add");
+    }
+    return (
+        <>
+        <Header />
+        <div className="col-sm-6 offset-sm-3" >
+            
+            <h1>User Sign Up</h1>
+            <input type="text" value={name} placeholder="name" onChange={(e)=>setName(e.target.value)} className="form-control" />
+            <br />
+            <input type="text" value={password} placeholder="password" onChange={(e)=>setPassword(e.target.value)} className="form-control" />
+            <br />
+            <input type="text" value={email} placeholder="email" onChange={(e)=>setEmail(e.target.value)} className="form-control" />
+            <br />
+            <button onClick={signUp} className="btn btn-primary" >Sign UP</button>
+        </div>
+        </>
+    )
+}
+
+export default Register;
